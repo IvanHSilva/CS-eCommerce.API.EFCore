47c138f1606f8fcf35d634fc1d9dbc3cfd9356cc
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-18 12:56:00 -0300

    User Model

---
+++ eCommerce.Models/User.cs
@@ -0,0 +1,30 @@
+﻿namespace eCommerce.Models {
+    public class User {
+        // Attributes
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? EMail { get; set; }
+        public string? Password { get; set; }
+        public string? Gender { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public string? Filiation { get; set; }
+        public string? Situation { get; set; }
+        public DateTime RegDate { get; set; }
+
+        // Constructors
+        public User() { }
+
+        public User(int id, string name, string eMail, string gender, string rg, string cpf, string filiation, string situation, DateTime regDate) {
+            Id = id;
+            Name = name;
+            EMail = eMail;
+            Gender = gender;
+            RG = rg;
+            CPF = cpf;
+            Filiation = filiation;
+            Situation = situation;
+            RegDate = regDate;
+        }
+    }
+}
