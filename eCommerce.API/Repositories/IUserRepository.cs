46686dab9024524b8148bed290585f8f8b7f235a
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-19 21:33:44 -0300

    IUserRepository Interface

---
+++ eCommerce.API/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories {
+    public interface IUserRepository {
+
+        List<User> GetUsers();
+        User GetUser(int id);
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int id);
+    }
+}
