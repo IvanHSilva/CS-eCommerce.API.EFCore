336ded162e268a2803785e0a4ee5084de6642f08
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-19 21:41:21 -0300

    UserRepository

---
+++ eCommerce.API/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories {
+    public class UserRepository : IUserRepository {
+
+        // User list
+        public static List<User> _db = new List<User>();
+
+        public List<User> GetUsers() {
+            return _db;
+        }
+
+        public User GetUser(int id) {
+            return _db.FirstOrDefault(x => x.Id == id)!;
+        }
+
+        public void InsertUser(User user) {
+            _db.Add(user);
+        }
+        
+        public void UpdateUser(User user) {
+            _db.Remove(GetUser(user.Id));
+            _db.Add(user);
+        }
+
+        public void DeleteUser(int id) {
+            _db.Remove(GetUser(id));
+        }
+    }
+}
