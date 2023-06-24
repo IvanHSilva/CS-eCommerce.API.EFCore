987d4124e454240de1d8dd6fe1d56cdcb81dd48f
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-06-24 13:31:10 -0300

    Departments EF Seeds

--- eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce.API/Database/eCommerceContext.cs
@@ -18,5 +18,17 @@ namespace eCommerce.API.Database {
         public DbSet<Contact> Contacts { get; set; } = null!;
         public DbSet<Address> Addresses { get; set; } = null!;
         public DbSet<Department> Departments { get; set; } = null!;
+
+        // EF Seeds
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 1, Name = "Eletrônicos" },
+                new Department { Id = 2, Name = "Livros" },
+                new Department { Id = 3, Name = "Informática" },
+                new Department { Id = 4, Name = "Moda" },
+                new Department { Id = 5, Name = "Beleza" },
+                new Department { Id = 6, Name = "Cozinha" }
+             );
+        }
     }
 }
